3f14d2e8922ef8fc7fc14253709e05a0dee67545
Jason <jasonppoole@gmail.com>
2017-03-21 15:44:35 -0400

    update

--- Jason/local_to_ldif.py
+++ Jason/local_to_ldif.py
@@ -62,7 +62,7 @@ with open("/etc/passwd", "r") as pw:
                 p.extend(re.split(':', line))
                 if p[0] not in shadow and p[2] >= 100:
                         passwd[p[0]] = p
-			#passwd[p[0]][1] = shadow[p[0]][1]
+			#passwd[p[0],1] = shadow[p[0],1]
 
 try:
         open("/etc/group", "r")
@@ -80,11 +80,33 @@ with open("/etc/group", "r") as gp:
                 if [3] in g and g[2] >= 100:
                         group[g[0]] = g
  
-
 try:
-        open(host +'.ldif', "w")
+        open(host + ".ldif", "w")
 except IOError:
         print("Opening ldif failed")
 	quit()
 
- 
+with open(host + ".ldif", "w") as ld:
+	ld.write("dn: " + "".join(str(x) for x in dc2) + "dc=" + dc1 + "\n")
+	ld.write("objectClass: dcObject \n")
+	ld.write("objectClass: organization \n")
+	ld.write("dc: " + domain + "\n")
+	ld.write("o: "+ domain + "\n")
+	ld.write("\n\n")
+
+	ld.write("dn: ou=Users, " + "".join(str(x) for x in dc2) + "dc=" + dc1 + "\n")
+	ld.write("ou: Users \n")
+	ld.write("objectClass: top \n")
+	ld.write("objectClass: organzationalUnit \n")
+	ld.write("\n\n")
+
+	ld.write("dn: ou=Groups," + "".join(str(x) for x in dc2) + "dc=" + dc1 + "\n")
+	ld.write("ou: Groups \n")
+	ld.write("objectClass: top \n")
+	ld.write("objectClass: organizationalUnit \n")
+	ld.write("\n\n")
+
+#for key in passwd:
+#	if len(passwd[[key],4]) == 0:
+#		passwd[key[4]] = key
+#	name = re.split(' ', passwd.get([key],[4]))
