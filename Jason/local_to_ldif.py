43152458a35a015cddbd9f6db86d63acaee13bc1
Jason <jasonppoole@gmail.com>
2017-03-21 14:53:54 -0400

    update

--- Jason/local_to_ldif.py
+++ Jason/local_to_ldif.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
+
 import socket
 import os
 import re
 
+fqdn = re.split('\W+', socket.gethostname())
+
+domain = fqdn[1]
+host = fqdn[0]
+dc1 = fqdn[-1]
+
+shadow = {}
+passwd = {}
+group = {}
+
 if os.getuid() != 0:
 	quit()
-
-fqdn = re.split('\W+', socket.gethostname())
 
 if fqdn[-1] == 0 or fqdn[1] == 'localdomain':
 	userinput = raw_input("FQDN could not be determined automatically. Please enter FQDN: ")
@@ -16,13 +25,42 @@ if fqdn[-1] == 0 or fqdn[1] == 'localdomain':
 	else:
 		fqdn = re.split('\W+', userinput)
 
-domain = fqdn[1]
-host = fqdn[0]
-dc2 = fqdn[-1]
-
 for i in fqdn[1:-1]:
-	dc = ["dc=" + i + ","]
+	dc2 = ["dc=" + i + ","]
 
 print "Using " + host + " as Hostname. Output will be in " + host +  ".ldif \n"
-print "Using " + "".join(str(x) for x in dc) + "dc="+ dc2 + " as LDAP suffix. \n"
+print "Using " + "".join(str(x) for x in dc2) + "dc="+ dc1 + " as LDAP suffix. \n"
+
+try:
+	open("/etc/shadow", "r")
+except IOError:
+	print("Opening shadow failed")	
+	quit()
+
+with open("/etc/shadow", "r") as sf:
+	for line in sf:
+		line.rstrip() 
+		s = []
+		s.extend(re.split(':', line))
+		if s[1] != '!!' and s[1] != '*':
+			shadow[s[0]] = s
+			print shadow
+try:
+        open("/etc/passwd", "r")
+except IOError:
+        print("Opening passwd failed")        
+	quit()
+
+
+try:
+        open("/etc/group", "r")
+except IOError:
+        print("Opening group failed")        
+	quit()
+
+try:
+        open(host +'.ldif', "w")
+except IOError:
+        print("Opening ldif failed")
+	quit()
 
