f142bdf7289fb5a6cb6773ee62bd7bf354789a79
Jason <jasonppoole@gmail.com>
2017-03-21 11:57:10 -0400

    adding python ldif script

---
+++ Jason/local_to_ldif.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import socket
+import os
+import re
+
+if os.getuid() != 0:
+	quit()
+
+fqdn = re.split('\W+', socket.gethostname())
+
+if fqdn[-1] == 0 or fqdn[1] == 'localdomain':
+	userinput = raw_input("FQDN could not be determined automatically. Please enter FQDN: ")
+	userinput.rstrip()
+	if userinput == "":
+		print "Null string not accepted. Please try again. \n"
+	else:
+		fqdn = re.split('\W+', userinput)
+
+domain = fqdn[1]
+#convertlisttostring = "".join(str(x) for x in fqdn[1:2])
+dc = "dc=" + domain
+host = fqdn[0]
+
+print "Using " + host + "  as Hostname. Output will be in " + host +  ".ldif \n"
+print "Using " + dc + " as LDAP suffix. \n"
